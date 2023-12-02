50c04714d4c8f468332661cb6543d3ac9fc57f02
Ben <baguirre@luc.edu>
2023-12-02 15:26:55 -0600

    day 2 complete

--- day2/day2part2.js
+++ day2/day2part2.js
@@ -2,29 +2,27 @@ const fs = require('fs');
 const readFile = (path) => fs.readFileSync(path, { encoding: "utf8" })
 const input = readFile('./day2/input.txt').split('\n')
 
-
-let dict = {
-    red: 12,
-    green: 13,
-    blue: 14,
-}
-
 const cubeConundrum = (inputs) => {
     let res = []
     for (inp of inputs) {
         const inpGames = inp.split(/[:;]/)
-        let id = Number(inpGames[0].split(' ')[1]);
-        let checker = true;
+        let dict = {
+            green: [],
+            red: [],
+            blue: []
+        }
+
         for (let i = 1; i < inpGames.length; i++) {
             let colors = inpGames[i].trimStart().split(', ').map((col) => {
                 let arr = col.split(' ')
                 return [arr[1], arr[0]]
             })
-            if (!colors.every((arr) => arr[1] <= dict[arr[0]])) {
-                checker = false
-            }
+            colors.forEach((color) => {
+                dict[color[0]].push(Number(color[1]))
+            })
         }
-        if (checker) res.push(id)
+        let max = Object.values(dict).map((arr) => Math.max(...arr))
+        res.push(max.reduce((accum, cur) => accum * cur, 1))
     }
     return res.reduce((accum, cur) => accum + cur, 0)
 }
