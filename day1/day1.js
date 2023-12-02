b2f1b3cad46ab14d2c9237c275e812c7fba5661d
Ben <baguirre@luc.edu>
2023-12-01 19:15:19 -0600

    final res

--- day1/day1.js
+++ day1/day1.js
@@ -2,24 +2,54 @@ const fs = require('fs');
 const readFile = (path) => fs.readFileSync(path, { encoding: "utf8" })
 const input = readFile('./day1/input.txt').split('\n')
 
+const cases = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+const numToInt = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+
+const checker = (value) => {
+    const map = new Map()
+    for (num of cases) {
+        if (value.includes(num)) {
+            map.set(value.indexOf(num), numToInt[num])
+            map.set(value.lastIndexOf(num), numToInt[num])
+        }
+    }
+    return [...map.entries()]
+}
+
 function isNumeric(s) {
     return !isNaN(s - parseFloat(s));
 }
 
 const findNum = (start, string) => {
     let pointer = start ? 0 : string.length - 1
-    while (!isNumeric(string[pointer]) || !string[pointer]) {
+    while (!isNumeric(string[pointer]) && string[pointer]) {
         start ? pointer++ : pointer--
     }
-    return string[pointer]
+    return string[pointer] ? [pointer, string[pointer]] : null
 }
 
 const decipher = (inputs) => {
     let count = 0
     for (inp of inputs) {
-        count += Number(findNum(true, inp) + findNum(false, inp))
+        const currArr = checker(inp)
+        currArr.push(findNum(true, inp), findNum(false, inp))
+
+        let res = currArr.filter(x => x)
+        res.sort((a, b) => a[0] > b[0] ? 1 : -1)
+
+        count += Number(res[0][1] + res.at(-1)[1])
     }
     return count
 }
 
-const res = decipher(input)
+console.log(decipher(input))
