0ea75d853d8d8c6386f03ff0b10db3a46d243c3f
Ben <baguirre@luc.edu>
2023-12-10 15:47:53 -0600

    checking point

--- day5/day5-part2.js
+++ day5/day5-part2.js
@@ -2,17 +2,47 @@ const fs = require('fs');
 const readFile = (path) => fs.readFileSync(path, { encoding: "utf8" })
 const input = readFile('./day5/input.txt').split('\n').filter(x => x !== '')
 
-function isBetween(x, min, max) {
-    return x >= min && x <= max;
-}
+const checkRange = (rangeMin, rangeMax, checkMin, checkMax) => (rangeMin >= checkMin && rangeMin <= checkMax) || (rangeMax <= checkMax && rangeMax >= checkMin)
 
-const createSeeds = (seedsArr) => {
+const createSeedRanges = (seedsArr) => {
     let arr = []
     for (let i = 0; i < seedsArr.length; i += 2) {
-        arr.push([seedsArr[i], seedsArr[i + 1] + seedsArr[i]])
+        arr.push([seedsArr[i], seedsArr[i + 1] + seedsArr[i], false])
     }
     return arr
 }
+
+const createNewRange = (curRange, numLow, numMax, replacement, rangeArr) => {
+    let [low, high] = curRange
+    if (high > numMax) {
+        let newArr = [numMax + 1, high, false]
+        rangeArr.push(newArr)
+        high = numMax
+        lowDif = low - numLow
+        highDif = high - numLow
+        low = replacement + lowDif
+        high = replacement + highDif
+    } else if (low < numLow) {
+        let newArr = [low, numLow - 1, false]
+        rangeArr.push(newArr)
+        low = numLow
+        lowDif = low - numLow
+        highDif = high - numLow
+        low = replacement + lowDif
+        high = replacement + highDif
+    } else {
+        let dif = low - numLow
+        let difHigh = high - numLow
+        low = replacement + dif
+        high = replacement + difHigh
+    }
+    curRange[0] = low
+    curRange[1] = high
+    curRange[2] = true
+    return rangeArr
+}
+
+
 
 const seedMapping = (inputs) => {
     let seeds = inputs.splice(0, 1)
@@ -20,12 +50,14 @@ const seedMapping = (inputs) => {
     seeds = seeds[0].toString().split(' ').map((seed) => Number(seed))
     seeds.shift()
 
-    let ranges = createSeeds(seeds)
-    let finalArr = []
+    let ranges = createSeedRanges(seeds)
+
 
     for (inp of inputs) {
         if (inp.split(' ').length === 2) {
-            // seeds = seeds.map((seed) => [seed[0], false])
+            ranges = ranges.map(range => [range[0], range[1], false])
+            console.log(ranges.map(range => range[0]))
+
             continue
         }
 
@@ -33,16 +65,18 @@ const seedMapping = (inputs) => {
         let count = Number(inp[2])
         let num = Number(inp[1])
         let replace = Number(inp[0])
-
-        seeds.forEach((seed, index) => {
-            if (isBetween(seed[0], num, num + count) && !seed[1]) {
-                let dif = seed[0] - num
-                seeds[index][0] = dif + replace
-                seed[1] = true
+        for (range of ranges) {
+            if (checkRange(range[0], range[1], num, num + count - 1)) {
+                if (range[2] === true) continue
+                createNewRange(range, num, num + count, replace, ranges)
             }
-        })
+        }
+
     }
-    return Math.min(...seeds.map((seed) => Number(seed[0])))
+    return Math.min(...ranges.map(range => range[0]))
 }
+
+
+
 
 console.log(seedMapping(input))
