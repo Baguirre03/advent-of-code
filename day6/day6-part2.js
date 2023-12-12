4e947b73331f95d47e9c53d3e345a85471a7b0f3
Ben <baguirre@luc.edu>
2023-12-12 17:35:44 -0600

    part two done

---
+++ day6/day6-part2.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const readFile = (path) => fs.readFileSync(path, { encoding: "utf8" })
+const input = readFile('./day6/input.txt').split('\n')
+
+const boatRace = (input) => {
+    let res = input.map((l) => l.split(' ').filter(x => x != '').splice(1).join('')).map(x => Number(x))
+    let final = []
+
+    let time = res[0]
+    let record = res[1]
+    let cur = 0
+    let count = 0
+
+    while (cur < time) {
+        let distance = cur * (time - cur)
+        if (distance > record) count++
+        else if (count !== 0 && cur < distance) break
+        cur++
+    }
+    return count
+}
+
+
+console.log(boatRace(input))
