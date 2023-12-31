28a0cf0e0a9b7154c8e621cc65ba6766db6b81c1
Ben <baguirre@luc.edu>
2023-12-31 18:37:30 -0500

    part one done

--- day12/day12.js
+++ day12/day12.js
@@ -27,6 +27,7 @@ const checkString = (string, positions, checker) => {
     positions.forEach((position) => {
         string[position] = '#'
     })
+
     string = string.map(x => x === '?' ? '.' : x).join('')
     const hashes = Object.entries(string).map(x => x[1] === '#' ? +x[0] : null).filter(x => x || x === 0)
     const groups = createGroups(hashes).map(x => x.length)
@@ -35,44 +36,19 @@ const checkString = (string, positions, checker) => {
     else return false
 }
 
-function createCombos(questionMarks, replacements, combinations) {
-    let comboArr = []
-    let prev = []
-
-    while (combinations > 1) {
-        let curNum = getNumbers(questionMarks, replacements)
-        if (!prev.includes(String(curNum))) {
-            comboArr.push(curNum)
-            prev.push(String(curNum))
-            combinations--
-        }
-    }
-
-    return comboArr
+function choose(arr, k, prefix = []) {
+    if (k == 0) return [prefix];
+    return arr.flatMap((v, i) =>
+        choose(arr.slice(i + 1), k - 1, [...prefix, v])
+    );
 }
 
-function getNumbers(questionMarks, replacements, nums = []) {
-    if (nums.length === replacements) return nums.sort((last, cur) => last - cur)
-
-    let curNum = questionMarks[Math.floor(Math.random() * questionMarks.length)]
-    if (nums.includes(curNum)) {
-        return getNumbers(questionMarks, replacements, nums)
-    } else {
-        nums.push(curNum)
-        return getNumbers(questionMarks, replacements, nums)
-    }
-}
-
-function decipher(pattern, questionMarks, replacements, combos, checker, prev = []) {
+function createCombos(questionMarks, replacements, pattern, checker) {
     let count = 0
-    let optionsArr = createCombos(questionMarks, replacements, combos)
-    console.log(optionsArr)
-
-    if (!prev.includes(String(numsReplacement))) {
-        combos--
-        prev.push(String(numsReplacement))
-        if (checkString(pattern.split(''), numsReplacement, checker)) count++
-    }
+    const optionsArr = choose(questionMarks, replacements)
+    optionsArr.forEach((option) => {
+        if (checkString(pattern.split(''), option, checker)) count++
+    })
 
     return count
 }
@@ -85,9 +61,8 @@ const hotSprings = (input) => {
         let [questionMarks, hashes] = [Object.entries(pattern).map((x) => x[1] === '?' ? +x[0] : null).filter(x => x || x === 0), Object.entries(pattern).map((x) => x[1] === '#' ? +x[0] : null).filter(x => x || x === 0).length]
 
         let replacements = numbers.split(',').map(x => +x).reduce((acum, cur) => acum + cur) - hashes
-        let combos = math.combinations(questionMarks.length, replacements)
+        let count = createCombos(questionMarks, replacements, pattern, numbers.split(',').map(x => +x))
 
-        let count = decipher(pattern, questionMarks, replacements, combos, numbers.split(',').map(x => +x))
         res.push(count)
     }
 
